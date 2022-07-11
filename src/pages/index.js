a5e18e75fbb67620f3a94ae6d1db4a1b78372798
Nicolas Young <nzkb24@gmail.com>
2022-07-10 21:33:45 -0500

    adjusted style

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,27 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Nicolas Young</title>
+        <title>Nicolas Young</title>{" "}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="src/public/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="src/public/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../public/favicon-16x16.png"
+        />
+        <link rel="manifest" href="src/public/site.webmanifest" />
+        <link rel="icon" href="src/public/favicon.ico" />
         <meta name="description" content="portfolio" />
-        <link rel="icon" href="..public/favicon.ico" />
       </Head>
 
       <Header />
@@ -27,28 +45,59 @@ export default function Home() {
       <footer>
         <section>
           <h3 className="title">Other places to find me!</h3>
-
-          <div>{/* links to git linkedin and twitter go here */}</div>
+          <ul className="about-section">
+            <li>
+              <a
+                href="https://github.com/maddnty"
+                target="_blank"
+                rel="noreferrer"
+                className="contact-link"
+              >
+                github
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/n_colasyoung"
+                target="_blank"
+                rel="noreferrer"
+                className="contact-link"
+              >
+                twitter
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/nicolas-young-818560227/"
+                target="_blank"
+                rel="noreferrer"
+                className="contact-link"
+              >
+                linkedin
+              </a>
+            </li>
+          </ul>
         </section>
+        <contact />
         <section>
           <div className="form-container">
             <form>
               <h3 className="title">You can slide right here!</h3>
-                <div className="form-item">
-                  <label>E-mail</label>
-                  <input type="email" required />
-                </div>
-                <div className="form-item">
-                  <label>Name</label>
-                  <input type="text" required />
-                </div>
-                <div className="form-item">
-                  <label>Message</label>
-                  <textarea></textarea>
-                </div>
-                <button type="submit" className="submit-btn">
-                  Send!
-                </button>
+              <div className="form-item">
+                <label>E-mail</label>
+                <input type="email" required />
+              </div>
+              <div className="form-item">
+                <label>Name</label>
+                <input type="text" required />
+              </div>
+              <div className="form-item">
+                <label>Message</label>
+                <textarea></textarea>
+              </div>
+              <button type="submit" className="submit-btn">
+                Send!
+              </button>
             </form>
           </div>
         </section>
